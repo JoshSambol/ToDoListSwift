06164ce5acc39ad9d31cf3dd5586b9afe44da1ae
Joshsambol <Joshua.sambol14@gmail.com>
2025-05-30 21:35:55 -0400

    Todo list uses the "new" button which makes a textfield and button. when you enter text and submit it uses a state to update a list. the list each have a toggle switch, where when you toggle it crosses out. The list uses a foreach which uses bindings so the delete doesnt work. I will change that in the future tho.

--- Todo list/Todo list/ContentView.swift
+++ Todo list/Todo list/ContentView.swift
@@ -6,16 +6,75 @@
 //
 
 import SwiftUI
-
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+struct Item: Identifiable {
+    let id = UUID()
+    var item: String
+    var done: Bool
+}
 struct ContentView: View {
+    @State private var itemList:[Item] = []
+    @State private var newItemName: String = ""
+    @State private var showNew = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                HStack{
+                    EditButton()
+                    Spacer()
+                }
+                
+                Text("To Do List")
+                    .bold()
+                    .font(.title)
+                
+                HStack {
+                    Spacer()
+                    Button(showNew ? "Cancel" : "New") {
+                        showNew.toggle()
+                        if !showNew{
+                            newItemName = ""
+                        }
+                    }
+                }
+                .padding()
+            }
+            if showNew {
+                HStack{
+                    TextField("Enter new item", text: $newItemName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                    Button("Add") {
+                        if !newItemName.isEmpty {
+                            let newItem = Item(item: newItemName, done: false)
+                            itemList.append(newItem)
+                            newItemName = ""
+                            showNew = false
+                        }
+                    }
+                }
+            }
+            List{
+                ForEach($itemList) { $item in
+                    Toggle(isOn: $item.done){
+                        Text(item.item)
+                            .strikethrough(item.done)
+                            .foregroundStyle(item.done ? Color.gray : .black)
+                    }
+                }
+                .onDelete { indexSet in
+                    itemList.remove(atOffsets:indexSet)}
+                .padding()
+            }
         }
-        .padding()
+        .onTapGesture {
+            hideKeyboard()
+        }
+        
     }
 }
 
