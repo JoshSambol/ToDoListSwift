09318f2628f9fde2968fbba53324490cdaafb1f5
Joshsambol <Joshua.sambol14@gmail.com>
2025-05-30 20:06:19 -0400

    Create proj

---
+++ Todo list/Todo list/Todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_listApp.swift
+//  Todo list
+//
+//  Created by Joshua Sambol on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct Todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
